27a858ba40b71431bf726eefcb94e7e3bc15f0d1
d0lev <dolevictory@gmail.com>
2020-11-23 17:44:50 +0200

    Init

--- src/api/directed_weighted_graph.java
+++ src/api/directed_weighted_graph.java
@@ -5,7 +5,7 @@ import java.util.Collection;
  * The interface has a road-system or communication network in mind - 
  * and should support a large number of nodes (over 100,000).
  * The implementation should be based on an efficient compact representation 
- * (should NOT be based on a n*n matrix). Kanye West Yezzy
+ * (should NOT be based on a n*n matrix). Kanye West Yezzey
  *
  */
 
