30c67f2a6f690243d52aa0569571d37f1cc0ad56
Dor Azaria <46644036+DorAzaria@users.noreply.github.com>
2020-12-07 18:58:57 +0200

    init gui

--- src/gameClient/gui/ourPanel.java
+++ src/gameClient/gui/ourPanel.java
@@ -3,6 +3,7 @@ import api.*;
 import javax.swing.*;
 import java.awt.*;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class ourPanel extends JPanel {
@@ -26,11 +27,21 @@ public class ourPanel extends JPanel {
     try {
         graph_algo.load("A0");
         graph = graph_algo.getGraph();
+        for(node_data current : graph.getV()) {
+            drawNode(g, current);
+        }
     }
-
     catch (FileNotFoundException f){
         f.printStackTrace();
     }
 
    }
+
+   private void drawNode(Graphics g, node_data current) {
+        geo_location geo = current.getLocation();
+        g.setColor(new Color(73,155,84));
+        g.fillOval((int)geo.x(),(int)geo.y(),20,20);
+        g.drawString(""+current.getKey(),(int)geo.x()+10,(int)geo.y()+10);
+   }
+
 }
